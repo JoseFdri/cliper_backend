9ffb41aadbe151d62e3e44984bd2259a5c4dc9d8
Jose Rodriguez <jose.guillen@Dynata.com>
2020-01-27 22:29:13 -0500

    setting up the project

--- routes.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const room = require('./controllers/room')
-
-module.exports = function (app) {
-    app.get('/create-room', room.createRoom)
-}
--- sockets.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const editor = require('./sockets/editor');
-
-module.exports = function (io) {
-    editor(io);
-}
